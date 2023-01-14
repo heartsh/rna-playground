47a56698ada15dbf08cdaf723efdb968497c1288
heartsh <heartsh@heartsh.io>
2023-01-14 12:08:59 +0900

    Add "docker-compose" service for macOS

--- docker_compose_wrapper.sh
+++ docker_compose_wrapper.sh
@@ -5,12 +5,12 @@ shift
 
 case $SUBCOMMAND in
   win)
-    docker-compose up win -d && sleep 3 && docker ps -l \
+    docker-compose up -d win && sleep 3 && docker ps -l \
     | grep -q "(healthy)" || echo "Warning: some package(s) cannot be installed properly"
     docker rm $(docker ps -lq) -f > /dev/null
     ;;
   mac)
-    docker-compose up mac -d && sleep 3 && docker ps -l \
+    docker-compose up -d mac && sleep 3 && docker ps -l \
     | grep -q "(healthy)" || echo "Warning: some package(s) cannot be installed properly"
     docker rm $(docker ps -lq) -f > /dev/null
     ;;
